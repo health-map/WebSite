46bf77c5ec4b9440994908d3d0209dfde0a1e017
Leonardo Larrea <leonardo@shippify.co>
2019-08-11 11:44:57 -0500

    anonymous session

--- src/middlewares/auth/index.js
+++ src/middlewares/auth/index.js
@@ -1,10 +1,17 @@
+const User = require('./../../models/user');
 module.exports = (req, res, next)=>{
     if(req.session == null || req.session.user == null ){
       console.log('req.method:',req.method)
       if(req.method === 'get' || req.method === 'GET'){ // right way to solve this
-        return res.redirect('/login');
+        return User.login({ email: 'anonymous@healthmap.com', password: '1234' }, (error, users)=>{
+          if(error){
+            return res.status(error.statusCode).json({ code: error.code, message: error.message})
+          }
+          next();
+          return;
+        })
       }
-      return res.status(403).message({ code: 'UA', message: 'Unauthenticated'})
+      return res.status(403).json({ code: 'UA', message: 'Unauthenticated'})
     }
     next();
 }
